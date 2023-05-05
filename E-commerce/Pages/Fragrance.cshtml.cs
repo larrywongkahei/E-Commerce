3b4d05936766ea3f482d220d3331cb83e76b43b2
Maryhei <makemak123@gmail.com>
2023-05-05 20:06:24 +0100

    Finished setting up fragrances page

--- E-commerce/Pages/Fragrance.cshtml.cs
+++ E-commerce/Pages/Fragrance.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace E_commerce.Pages
 {
     public class FragranceModel : PageModel
     {
-        public void OnGet()
+        
+        public IEnumerable<Product> FragranceFromAPI { get; set; }
+
+        public ProductsAPIService APIService;
+
+        public FragranceModel(ProductsAPIService apiservice)
         {
+            APIService = apiservice;
+        }
+        public async Task OnGet()
+        {
+            var allData = await APIService.GetProductsFromAPI();
+            FragranceFromAPI = from data in allData.Product where data.Category == "fragrances" select data;
         }
     }
 }
