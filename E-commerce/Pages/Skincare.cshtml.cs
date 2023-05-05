d3f0833f2fdf2c5c3b5055e53ccf8c048a38960a
Maryhei <makemak123@gmail.com>
2023-05-05 22:36:44 +0100

    Finished setting up skincare products page

--- E-commerce/Pages/Skincare.cshtml.cs
+++ E-commerce/Pages/Skincare.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,20 @@ namespace E_commerce.Pages
 {
     public class SkincareModel : PageModel
     {
-        public void OnGet()
+
+        public IEnumerable<Product> SkinCareFromAPI { get; set; }
+
+        public ProductsAPIService APIService;
+
+        public SkincareModel(ProductsAPIService apiservice)
         {
+            APIService = apiservice;
+        }
+
+        public async Task OnGet()
+        {
+            var allData = await APIService.GetProductsFromAPI();
+            SkinCareFromAPI = from data in allData.Product where data.Category == "skincare" select data;
         }
     }
 }
