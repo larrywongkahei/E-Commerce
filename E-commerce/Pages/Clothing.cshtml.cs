fb6e28bf173dafb4ba0062f00abb84a2c77edaa7
Maryhei <makemak123@gmail.com>
2023-05-05 22:30:16 +0100

    Added Css of the clothing page :

--- E-commerce/Pages/Clothing.cshtml.cs
+++ E-commerce/Pages/Clothing.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
