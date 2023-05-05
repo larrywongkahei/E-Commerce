c22c55dde5bfd32e8aeb02ce9add865d9bbde0b9
Maryhei <makemak123@gmail.com>
2023-05-06 00:58:38 +0100

    Finished setting up smartphone page

--- E-commerce/Pages/SmartPhone.cshtml.cs
+++ E-commerce/Pages/SmartPhone.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace E_commerce.Pages
 {
     public class SmartPhoneModel : PageModel
     {
-        public void OnGet()
+        public ProductsAPIService productAPIService;
+
+        public IEnumerable<Product> SmartPhones { get; set; }
+
+        public SmartPhoneModel(ProductsAPIService apiService)
         {
+            productAPIService = apiService;
+        }
+
+        public async Task OnGet()
+        {
+            var allData = await productAPIService.GetProductsFromAPI();
+            SmartPhones = from each in allData.Product where each.Category == "smartphones" select each;
         }
     }
 }
