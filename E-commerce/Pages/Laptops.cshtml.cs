1cc0ee44c30ca1b3b873e10b9b11112dc4e112b5
Maryhei <makemak123@gmail.com>
2023-05-06 01:00:54 +0100

    Finished setting up laptops page

--- E-commerce/Pages/Laptops.cshtml.cs
+++ E-commerce/Pages/Laptops.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace E_commerce.Pages
 {
     public class LaptopsModel : PageModel
     {
-        public void OnGet()
+        public ProductsAPIService productAPIService;
+
+        public IEnumerable<Product> Laptops { get; set; }
+
+        public LaptopsModel(ProductsAPIService apiService)
         {
+            productAPIService = apiService;
+        }
+
+        public async Task OnGet()
+        {
+            var allData = await productAPIService.GetProductsFromAPI();
+            Laptops = from each in allData.Product where each.Category == "laptops" select each;
         }
     }
 }
