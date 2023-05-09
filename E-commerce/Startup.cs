858c920fc836daa8c5c0bd59a2e3ee33fc761fe9
Maryhei <makemak123@gmail.com>
2023-05-09 22:16:33 +0100

    Set up for session

--- E-commerce/Startup.cs
+++ E-commerce/Startup.cs
@@ -28,6 +28,14 @@ namespace E_commerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
