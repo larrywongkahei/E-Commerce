dcf1cb2188b18cc7b4df4c0607f1fd97bccbcc4c
Maryhei <makemak123@gmail.com>
2023-05-10 01:36:01 +0100

    Set up the controller page of the searchResult page

--- E-commerce/Pages/SearchResult.cshtml.cs
+++ E-commerce/Pages/SearchResult.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,33 @@ namespace E_commerce.Pages
 {
     public class SearchResultModel : PageModel
     {
-        public void OnGet()
+        public JsonFileService jsonfileservice;
+
+        public ProductsAPIService productapiservice;
+
+        public IEnumerable<Product> ProductsFromAPI { get; set; }
+
+        public IEnumerable<Model.Products> ProductsFromJson { get; set; }
+
+        public string SearchValue { get; set; }
+
+        public Dictionary<string, string> data { get; set; }
+
+        public SearchResultModel(JsonFileService fileservice, ProductsAPIService apiservice)
         {
+            jsonfileservice = fileservice;
+            productapiservice = apiservice;
+        }
+
+        public async Task OnGet(Dictionary<string, string> param)
+        {
+            data = param;
+            SearchValue = data.First().Value;
+            var allDataFromAPI = await productapiservice.GetProductsFromAPI();
+            ProductsFromAPI = from each in allDataFromAPI.Product where each.Title.Contains(SearchValue) select each;
+            var allDataFromJson = jsonfileservice.GetAll();
+            ProductsFromJson = from each in allDataFromJson where each.Title.Contains(SearchValue) select each;
+
         }
     }
 }
