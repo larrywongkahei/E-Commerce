4602bcbd3add01aa08e3b8fe3ae1c7a93b0a4f1d
Maryhei <makemak123@gmail.com>
2023-05-14 12:51:49 +0100

    Finished setting up the confirmadded page logic

---
+++ E-commerce/Pages/ConfirmAdded.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_commerce.DataAPI;
+using E_commerce.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_commerce.Pages
+{
+    public class ConfirmAddedModel : BaseModel
+    {
+        public JsonFileService fileservice { get; set; }
+
+        public ProductsAPIService apiService { get; set; }
+
+        public ConfirmAddedModel(JsonFileService jsonFileService, ProductsAPIService productsAPIService)
+        {
+            fileservice = jsonFileService;
+            apiService = productsAPIService;
+        }
+
+        public async Task OnGetAsync(string name)
+        {
+            var jsonData = fileservice.GetAll();
+            var apiData = await apiService.GetProductsFromAPI();
+            if(apiData.Product.First(each => each.Title == name) != null)
+            {
+                Product productToAdd = apiData.Product.First(each => each.Title == name);
+            }
+            else
+            {
+                Model.Products productToAdd = jsonData.First(each => each.Title == name);
+            }
+
+        }
+    }
+}
